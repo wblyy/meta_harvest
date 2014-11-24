90960319fb331880db375dfdf0b501d9665f16b2
Balloon_Wang <wblyy0911@gmail.com>
2014-11-24 18:33:38 +0800

    集体加

--- no_company.py
+++ no_company.py
@@ -30,25 +30,25 @@ for lrow in list(reader):
         if '好声音' in album:
             #梦响强音
             meta.update_album_company(album,'梦响强音')
-        elif 'M-net' in album
+        elif 'M-net' in album:
             meta.update_album_company(album,'SM')
-        elif '好歌曲' in album
+        elif '好歌曲' in album:
             meta.update_album_company(album,'通力时代')
-        elif '雪碧' in album
+        elif '雪碧' in album:
             meta.update_album_company(album,'可口可乐')
-        elif '最美和声' in album
+        elif '最美和声' in album:
             meta.update_album_company(album,'北京卫视')
-        elif '我是歌手' in album 
+        elif '我是歌手' in album:
             meta.update_album_company(album,'湖南卫视')
-        elif '华语单曲榜' in album
+        elif '华语单曲榜' in album:
             meta.update_album_company(album,'KKBOX')
-        elif '在线热搜（华语）系列' in album
+        elif '在线热搜（华语）系列' in album:
             meta.update_album_company(album,'QQ音乐')
-        elif '新歌速递' in album
+        elif '新歌速递' in album:
             meta.update_album_company(album,'QQ音乐')
-        elif '最强音' in album
+        elif '最强音' in album:
             meta.update_album_company(album,'湖南卫视')
-        elif '直通春晚' in album
+        elif '直通春晚' in album:
             meta.update_album_company(album,'恒大音乐')
 
 
