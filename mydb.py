5527fa7f0b0face52cfaedc9b787a3270507c67f
Balloon_Wang <wblyy0911@gmail.com>
2014-11-25 12:16:21 +0800

    only-by song

--- mydb.py
+++ mydb.py
@@ -55,8 +55,8 @@ class Tiedb(Mydb):
         return self._query_row('select lyricid from song_info_163music where title=%s and artist=%s limit 1', (song, artist))
     def get_lyric_id_qqmusic(self,song, artist):
         return self._query_row('select lyricid from song_info_qqmusic where title=%s and artist=%s limit 1', (song, artist))
-    def get_lyric_id_xiami(self,song, artist):
-        return self._query_row('select lyricid from song_info_xiami where title=%s and artist=%s limit 1', (song, artist))
+    def get_lyric_id_xiami(self,song):
+        return self._query_row('select lyricid from song_info_xiami where title=%s limit 1', (song, ))
     def get_lyric_by_id(self,id):
         return self._query_row('select lyric from lyrics where id=%s ', (id, ))
 
